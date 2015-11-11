f6ab91f6a4df2908cac6544092df599a44fbca8f
Jackie_Wang <Hotabbit@163.com>
2015-11-11 15:13:19 -0800

    add ss.sh

---
+++ ss.sh
@@ -0,0 +1,48 @@
+Last login: Fri Nov  6 16:55:21 on ttys000
+WangJackiedeMacBook-Pro-2:~ jackie_wang$ vim
+
+[1]+  Stopped                 vim
+WangJackiedeMacBook-Pro-2:~ jackie_wang$ vi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+echo "running rc.groundhog.postprocess as 'id'"
+echo "rc.groundhog.postprocess running my commands"
+/Users/gdlocal/myInstallerScript.sh
+echo "done with rc.groundhog.postprocess as 'id'"
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --
